2ad290bc92c8e36287869272d88ad94c586674af
smiley128 <smiley128@users.noreply.github.com>
2014-07-23 01:29:53 +1000

    Update HealingStaff.ino
    Cleaned up the code. Add more comments, Added another mode 'All White'

--- HealingStaff.ino
+++ HealingStaff.ino
@@ -1,81 +1,88 @@
 #include <Adafruit_NeoPixel.h>
-#define PIN 2
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(48, PIN, NEO_GRB + NEO_KHZ800);
+#define ButtonPin 0
+#define LEDpin 1
+#define NeoPixelPin 2
+#define Brightness 128 // Brightness (For all modes except mode 2 'All White')
+#define HoldTime 1500 // Button hold time to change modes
+#define interval 30000 // Interval for healing 30 sec
+#define GlobalTickDelay 4 //The length of time between each loop
 
-byte buttonPushCounter = 0;   // counter for the number of button presses
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(48, NeoPixelPin, NEO_GRB + NEO_KHZ800);
+
+#define n_modes 4 // number of modes 
 boolean buttonState = 0;         // current state of the button
 boolean lastButtonState = 1;     // previous state of the button
 boolean pressed = false;
 boolean healing = false;
 unsigned long previousMillis = 0;
 unsigned long previousMillishold = 0;
-#define interval 30000 //30 sec
-unsigned int cycle = 0;
+unsigned long cycle = 0; 
 byte mode = 0;
-#define n_modes 3
 
 void setup() {
   strip.begin();
-  pinMode(3, INPUT);
-  pinMode(0, INPUT_PULLUP);
-  pinMode(1, OUTPUT);
-  randomSeed(analogRead(3));
-  strip.setBrightness(128);
-  strip.show();
+  pinMode(ButtonPin, INPUT_PULLUP);
+  pinMode(LEDpin, OUTPUT);
 
-  while (buttonPushCounter < 1){
+  while (!pressed){ //rainbow standby 
     strip.setPixelColor(random(strip.numPixels()), Wheel((cycle+random(64))/2));
-    strip.setPixelColor(random(strip.numPixels()),0x000000);
+    strip.setPixelColor(random(strip.numPixels()),0);
     share();
   }
+  randomSeed(millis());
 }
 
 void loop() {
 
   switch (mode) {
-  case 0:
-    if(healing){
-      digitalWrite(1,1);
+  case 0: // healing and standby
+    if(healing){ //healing mode
+      digitalWrite(LEDpin,1);
       long t = millis() - previousMillis;
-      byte  p = map(t, 0, interval, 0, (strip.numPixels()+1));
+      byte p = map(t, 0, interval, 0, (strip.numPixels()+1));
       strip.setPixelColor(random(p,strip.numPixels()), Wheel((random(32)+96)));
       strip.setPixelColor(random(p), Wheel((random(42))));
       if(t > interval)
         healing = false;
     }
-    else{
-      digitalWrite(1,0);
+    else{ //standby mode
+      digitalWrite(LEDpin,0);
       strip.setPixelColor(random(strip.numPixels()), Wheel(random(64)+96));
     }
-   
-    if (pressed){
-    healing = !healing;
-    if(healing)
-      previousMillis = millis();
 
-    pressed = false;
-  }
+    if (pressed){
+      healing = !healing;
+      if(healing){
+        previousMillis = millis();
+      }
+      pressed = false;
+    }
+    strip.setPixelColor(random(strip.numPixels()),0);
+    break;  
+
+  case 1:// Flame 
+    strip.setPixelColor((cycle+random(3))%strip.numPixels(), Wheel(((((cycle+random(5))%32)+48+random(7))%256)));
+    strip.setPixelColor(random(strip.numPixels()),0);
     break;
-  case 1:
-    strip.setPixelColor((cycle+random(3))%64, Wheel(((((cycle+random(5))%32)+strip.numPixels()+random(7))%256)));
+
+  case 2: // All White
+    strip.setBrightness(255);
+    allStrip(0xFFFFFF);
     break;
-  case 2:
-  
+  case 3://Police Lights
+
     if (cycle%20 == 0)
       if (cycle%200 >= 100)
-      allStrip(0xFF0000);
+        allStrip(0xFF0000);
       else
-      allStrip(0x0000FF);
+        allStrip(0x0000FF);
     if ((cycle+10)%20 == 0)
-    allStrip(0x000000);
+      allStrip(0x000000);
     break;
   }
 
-if (mode < 2)
-strip.setPixelColor(random(strip.numPixels()),0);
-
-share();  
+  share();  
 
 }
 
@@ -86,26 +93,26 @@ void allStrip(uint32_t color){
 }
 
 void share(){  
-  delay(4);
+  delay(GlobalTickDelay);
   buttonCheck();
   cycle++;
   strip.show();
+  strip.setBrightness(128);
 }
 
 void buttonCheck(){
-  buttonState = digitalRead(0);
+  buttonState = digitalRead(ButtonPin);
 
   if (buttonState != lastButtonState) {
     if (buttonState == LOW) {
-      buttonPushCounter++;
       pressed = true;
       previousMillishold = millis();
     }
   }
   else{
     if (buttonState == LOW) {
-      long t = millis() - previousMillishold;
-      if(t > 2000){
+      unsigned long t = millis() - previousMillishold;
+      if(t > HoldTime){
         mode++;
         mode=mode%n_modes;
         previousMillishold = millis();
@@ -129,10 +136,3 @@ uint32_t Wheel(byte WheelPos) {
     return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
-
-
-
-
-
-
-
